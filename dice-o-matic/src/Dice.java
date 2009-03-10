ee1e6f2317f32e4de7e180c756fcc04253f3535a
jordane.marcel <jordane.marcel@8f85035a-0d82-11de-9c5c-7db7966a26d7>
2009-03-10 16:07:21 +0000

    Initial commit.

---
+++ dice-o-matic/src/Dice.java
@@ -0,0 +1,18 @@
+package fr.umlv.ig.misc;
+
+public interface Dice {
+
+    /**
+     * NOTE: a class implementing the Dice interface is not supposed to declare 
+     *       more than one constructor. This constructor is supposed to be annotated
+     *       with a String array containing a description of the dice followed by a 
+     *       description of the constructor's parameters.
+     */
+
+    
+    /**
+     * @return an integer in [1;6]
+     */
+    public int getValue();
+    
+}
