f8909e46b9bec4ae333b6a3f432502a87952d1b0
jordane.marcel <jordane.marcel@8f85035a-0d82-11de-9c5c-7db7966a26d7>
2009-03-15 21:17:36 +0000

    Initial commit.

--- dice-o-matic/src/fr/umlv/ig/misc/Main.java
+++ dice-o-matic/src/fr/umlv/ig/misc/Main.java
@@ -1,10 +1,13 @@
 package fr.umlv.ig.misc;
 
 import java.awt.Menu;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.WindowConstants;
 
 public class Main {
@@ -13,7 +16,22 @@ public class Main {
 		final JFrame f = new JFrame("Dice'o'matic");
 		f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		f.setSize(640, 480);
-		f.pack();
+		JMenuBar menuBar = new JMenuBar();
+		JMenu fileMenu = new JMenu("File");
+		JMenuItem loadJarItem = new JMenuItem("Load a Dice file");
+		JMenuItem quitItem = new JMenuItem("Quit");
+		quitItem.addActionListener(new ActionListener() {
+		
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				f.dispose();
+		
+			}
+		});
+		fileMenu.add(loadJarItem);
+		fileMenu.add(quitItem);
+		menuBar.add(fileMenu);
+		f.setJMenuBar(menuBar);
 		f.setVisible(true);
 	}
 	
